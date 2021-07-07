0eb6ce84f32b903457f6daf5effaca4f4772e762
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-07-07 15:35:41 +0530

    commit all files.

---
+++ demo3/src/main/java/demo3/ABC.java
@@ -0,0 +1,14 @@
+package demo3;
+
+public class ABC {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello");
+		System.out.print("wtmw");
+		System.out.print("nfu");
+		System.out.print("wtmw1");
+		System.out.print("nfu1");
+	}
+
+}
