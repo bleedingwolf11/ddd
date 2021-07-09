87b3a0dec461da4274cc47e5bd7b16bf0cf394b6
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-07-09 07:48:34 +0530

    cal

--- demo3/src/main/java/demo3/ABC.java
+++ demo3/src/main/java/demo3/ABC.java
@@ -10,5 +10,11 @@ public class ABC {
 		System.out.print("wtmw1");
 		System.out.print("nfu1");
 	}
-
+	public void calculate() {
+		int a[]= {1,2,3};
+		int sum=0;
+		for(int i=0;i<a.length;i++)	{
+			sum+=a[i];
+		}
+	}
 }
