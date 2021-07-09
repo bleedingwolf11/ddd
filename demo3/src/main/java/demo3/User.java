307f566e4bd3099a8754c1f12271953fef949cf9
bleedingwolf11 <56654525+bleedingwolf11@users.noreply.github.com>
2021-07-09 07:44:19 +0530

    cal

--- demo3/src/main/java/demo3/User.java
+++ demo3/src/main/java/demo3/User.java
@@ -3,4 +3,12 @@ package demo3;
 public class User {
 String user;
 String userop;
+
+public void calculate() {
+	int a[]= {1,2,3};
+	int sum=0;
+	for(int i=0;i<a.length;i++)	{
+		sum+=a[i];
+	}
+}
 }
